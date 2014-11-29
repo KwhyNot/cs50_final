cab20ee22b2e06ea7673bde255039d5d3a93ddf6
MurphyCP <christophermurphy@g.harvard.edu>
2014-11-29 17:05:50 -0500

    RGB conversion function added

--- functions.py
+++ functions.py
@@ -28,3 +28,23 @@ def getAverageScreenColor():
         green /= total
     os.remove("screenshot.png")
     return red, green, blue
+ 
+def rgbConvert(r, g, b):
+    """ Calculates the XY values (in the Hue format) from RGB values.
+        Returns the X and Y values of the color, as floats.
+    """
+    r = pow((r + 0.055) / (1.0 + 0.055), 2.4) if r > 0.04045 else r / 12.92
+    g = pow((g + 0.055) / (1.0 + 0.055), 2.4) if g > 0.04045 else g / 12.92
+    b = pow((b + 0.055) / (1.0 + 0.055), 2.4) if b > 0.04045 else b / 12.92
+    X = r * 0.649926 + g * 0.103455 + b * 0.197109
+    Y = r * 0.234327 + g * 0.743075 + b * 0.022598
+    Z = r * 0.000000 + g * 0.053077 + b * 1.035763
+    x = X / (X + Y + Z)
+    y = Y / (X + Y + Z)
+    return x, y
+    
+r, g, b = getAverageScreenColor()
+print (r, g, b)
+x, y = rgbConvert(r, g, b)
+print (x, y)
+
